f7ef2ba2c61b423c710249e800174b4bc0ea6110
unknown <you@example.com>
2022-08-23 10:05:48 +0530

    Parking Microservice added

---
+++ Parking System/Parking/Migrations/20220822095834_FirstMigrations.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parking.Migrations
+{
+    public partial class FirstMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Slot",
+                columns: table => new
+                {
+                    SlotID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Floor = table.Column<string>(nullable: false),
+                    IsParked = table.Column<bool>(nullable: false),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slot", x => x.SlotID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    VehicleNumber = table.Column<string>(maxLength: 10, nullable: false),
+                    Company = table.Column<string>(nullable: false),
+                    Model = table.Column<string>(nullable: false),
+                    Type = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.VehicleNumber);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parking",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleNumber = table.Column<string>(nullable: true),
+                    ParkingSlotsSlotID = table.Column<int>(nullable: true),
+                    ParkedTime = table.Column<DateTime>(nullable: false),
+                    UnparkedTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parking", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parking_Slot_ParkingSlotsSlotID",
+                        column: x => x.ParkingSlotsSlotID,
+                        principalTable: "Slot",
+                        principalColumn: "SlotID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Parking_Vehicle_VehicleNumber",
+                        column: x => x.VehicleNumber,
+                        principalTable: "Vehicle",
+                        principalColumn: "VehicleNumber",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parking_ParkingSlotsSlotID",
+                table: "Parking",
+                column: "ParkingSlotsSlotID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parking_VehicleNumber",
+                table: "Parking",
+                column: "VehicleNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parking");
+
+            migrationBuilder.DropTable(
+                name: "Slot");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
--- Parking System/Parking/Startup.cs
+++ Parking System/Parking/Startup.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Parking.Models;
+using Parking.Models.Repositoy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +27,12 @@ namespace Parking
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ParkingContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:SlotDb"]));
+            services.AddScoped<IParkingRepository<Models.Parking>, ParkingManager>();
             services.AddControllers();
+
+            //Swagger Configurations
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +50,14 @@ namespace Parking
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            //swagger Endpoints
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAPI");
             });
         }
     }
